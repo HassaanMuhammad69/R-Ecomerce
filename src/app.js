963c9f2a5f08940f89acc5c69e5836f3c024d439
Hassaan Muhammad <hassaans299@gmail.com>
2024-06-19 23:44:50 -0700

    first controller

--- src/app.js
+++ src/app.js
@@ -15,4 +15,11 @@ app.use(express.static("public"))
 app.use(cookieParser())
 
 
+//routes import
+import  useRouter  from './routes/user_routes.js'
+
+//routes decleration
+app.use("/api/v1/users",useRouter)
+
+
 export {app}
